abbccbff7688dd136493701e5dbc0445e65ccd7e
kushagra-aa <kushagraaagnihotri@gmail.com>
2021-04-22 13:38:29 +0530

    FIXED!! Login & Signup is working now!

--- 3-d-api/connection.js
+++ 3-d-api/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const con_url_user = "mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/3D?retryWrites=true&w=majority";
+const con_url_user = "mongodb+srv://kushagra:kgumatrt@cluster1.rommy.mongodb.net/3D?retryWrites=true&w=majority";
 
 
 mongoose.connect(con_url_user, { useNewUrlParser: true, useUnifiedTopology: true })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userservice: UserService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  validateUser(email, password) {
+    console.log(email, password);
+    this.userservice.getbyemail(email).subscribe(data => {
+      if (data) {
+        if (data['password'] == password) {
+          console.log('login success');
+          sessionStorage.setItem('user', JSON.stringify(data));
+          this.userservice.loggedIn = true;
+          this.router.navigate(['/']);
+        }
+        else {
+          console.log('password incorrect');
+        }
+      }
+      else {
+        console.log('user not found');
+      }
+    })
+  }
+
   showPassword = false;
 
   getInputType() {
